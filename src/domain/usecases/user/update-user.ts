0b3343a87cc2f97860ee7c922364b23d5b22278b
ihagofsantos <ihago.santos@compasso.com.br>
2025-05-17 11:08:40 -0300

    add bug o UpdateUserUseCase class to be fixed by amazon q developer

--- src/domain/usecases/user/update-user.ts
+++ src/domain/usecases/user/update-user.ts
@@ -8,14 +8,9 @@ export class UpdateUserUseCase {
 
     async execute({id, name, email}: UpdateUser.Input): Promise<void> {
         const user = await this.userRepository.getUserById(id);
-        const existingUser = await this.userRepository.getUserByEmail(email);
         
         if(!user) throw new NotFoundError("user not found");
 
-        if(existingUser && existingUser.id !== id) {
-            throw new ConflictError("Email already exists");
-        }
-        
         await this.userRepository.updateUser(id, name, email);
     } 
 }
